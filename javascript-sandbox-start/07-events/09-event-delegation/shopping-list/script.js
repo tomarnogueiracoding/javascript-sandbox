c800229be775cb5f958c32f55f597e8bed1de2c0
tomarnogueiracoding <tomarnogueiracoding@gmail.com>
2026-09-18 04:00:41 +0100

    events

---
+++ javascript-sandbox-start/07-events/09-event-delegation/shopping-list/script.js
@@ -0,0 +1,14 @@
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach((item) => {
+//   item.addEventListener('click', (e) => {
+//     e.target.remove();
+//   });
+// });
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.remove();
+  }
+});
--- javascript-sandbox-start/07-events/10-window-events/script.js
+++ javascript-sandbox-start/07-events/10-window-events/script.js
@@ -1,1 +1,20 @@
+// window.onload = () => {
+//   document.querySelector('h1').textContent = 'Hello World';
+// };
 
+window.addEventListener('load', () => {
+  console.log('page loaded');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM loaded');
+});
+
+console.log('Run me');
+
+document.querySelector('h1').textContent = 'Hello World';
+
+window.addEventListener('resize', () => {
+  document.querySelector('h1').textContent =
+    `Width: ${window.innerWidth} px, Height: ${window.innerHeight} px`;
+});
